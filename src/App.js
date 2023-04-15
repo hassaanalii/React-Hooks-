18938d625ffe55a83f57b820f35950158ed8565f
Hassaan Ali <hassaan ali>
2023-04-16 04:10:24 +0500

    Final changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [count1, setCount1] = useState(0)
+  const [count2, setCount2] = useState(0)
+  const clicked1 = () =>{
+    setCount1(count1+1);
+  }
+  const clicked2 = () =>{
+    setCount2(count2+1);
+  }
+  useEffect(()=>{
+    alert("hello")
+  },[count1])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={clicked1}>Press me</button>
+     <p>{count1}</p>
+     <button onClick={clicked2}>Press me</button>
+     <p>{count2}</p>
     </div>
   );
 }
